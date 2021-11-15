09801226f1fd5991062b7d14689e35ad5c774993
Rose <rosemwangi305@gmail.com>
2021-11-15 23:15:04 +0300

    counting pass
    counting pass and increment

---
+++ main.js
@@ -0,0 +1,17 @@
+// document.getElementById("count-el").innerText = 5;
+// let count = 0;
+// console.log(count);
+
+// function increment() {
+//     console.log("The button was clicked");
+// }
+
+//initialize
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    console.log("clicked");
+    countEl.innerHTML = count;
+}
