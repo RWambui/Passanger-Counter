c106ea24d50f49c3c4366e2e8377d283256ada0b
Rose <rosemwangi305@gmail.com>
2021-11-16 00:22:14 +0300

    plus equal technique

--- main.js
+++ main.js
@@ -11,8 +11,7 @@ let countEl = document.getElementById("count-el");
 let count = 0;
 
 function increment() {
-    count = +1;
-    console.log("clicked");
+    count += 1;
     countEl.innerHTML = count;
 }
 
